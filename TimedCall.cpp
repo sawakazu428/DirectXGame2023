d630fac2b5c069332f1537952c678d8f97d52803
sawada <k022g1063@g.neec.ac.jp>
2023-07-17 14:18:50 +0900

    応用

--- TimedCall.cpp
+++ TimedCall.cpp
@@ -1,1 +1,22 @@
 ﻿#include "TimedCall.h"
+
+TimedCall::TimedCall(std::function<void(void)> f, uint32_t time)
+{ 
+	std::function<void(void)> f = f;
+	time_ = time;
+}
+
+void TimedCall::Update()
+{ 
+	if (isComp)
+	{
+		return;
+	}
+	time_--;
+	if (time_ <= 0)
+	{
+		isComp = true;
+		// コールバック関数呼び出し
+		f();
+	}
+};
--- TimedCall.h
+++ TimedCall.h
@@ -1,2 +1,22 @@
 ﻿#pragma once
-class TimedCall {};
+#include "iostream"
+#include "functional"
+class TimedCall {
+public:
+	// コンストラクタ
+	TimedCall(std::function<void(void)> f, uint32_t time);
+	// 更新
+	void Update();
+	// 完了ならtrueを返す
+	bool IsFinished() { return isComp; }
+
+private:
+	// コールバック
+	std::function<void(void)> f;
+	// 残り時間
+	uint32_t time_;
+
+	// 完了フラグ
+	bool isComp = false;
+
+};  
