78c20ac2efafd0aa5862675507a2fd4369fab01d
sawada <k022g1063@g.neec.ac.jp>
2023-08-04 15:28:11 +0900

    01_02_確認課題

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2B_04_サワダカズキ_AL3");
+	win->CreateGameWindow(L"GC2B_04_サワダカズキ_AL3"); 
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
